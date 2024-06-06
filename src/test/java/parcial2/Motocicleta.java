31a9f9868e1358a0a953b0e6931842e51b59833e
Juan Acosta <juanestebanacostarodriguez@gmail.com>
2024-06-06 10:29:55 -0500

    mas

--- src/test/java/parcial2/Motocicleta.java
+++ src/test/java/parcial2/Motocicleta.java
@@ -5,7 +5,7 @@ package parcial2;
 public class Motocicleta extends Vehiculo{
     private TipoMotocicleta tipomotocicleta;
     
-    public Motocicleta(String marca, String modelo, int año) {
+    public Motocicleta(String marca, String modelo, int año, TipoMotocicleta tipoMotocicleta) {
         super(marca, modelo, año);
     }
     
